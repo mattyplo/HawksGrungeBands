ad835fd0709658322734c90eb4449cefd71e674f
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-04-03 09:05:48 -0700

    created 3 seperate queries to spotify and 3 subsequent calls to create appropriate spreadsheets

--- GrungeBandPopularity.py
+++ GrungeBandPopularity.py
@@ -4,18 +4,16 @@ import xlwt
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
-def writeTracksToExcel(query):
+def writeTracksToExcel(query, artist):
     wb = xlwt.Workbook()
     sheet = wb.add_sheet("New Sheet")
     count = 1
     for i in query['tracks']['items']:
-        print(i['name'] + ' ', i['popularity'])
-        name = i['name']
-        print(str(name))
-        sheet.write(count, 1, i['name'])
+        sheet.write(count, 0, i['name'])
+        sheet.write(count, 1, i['popularity'])
         count += 1
 
-    wb.save('newSheet.xls')
+    wb.save(artist + '.xls')
 
 # Setup authorization token to access spotify api
 client_credentials_manager = SpotifyClientCredentials(client_id='926b2b6950e745cc80d89ed6037db878', client_secret= 'd745522d16f747c79e15c2a4648b388f')
@@ -24,5 +22,9 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # search spotify for top fifty tracks by given artist
 results = sp.search(q='artist:' + 'alice in chains', type='track', limit=50)
+results2 = sp.search(q='artist:' + 'pearl jam', type='track', limit=50)
+results3 = sp.search(q='artist:' + 'soundgarden', type='track', limit=50)
 
-writeTracksToExcel(results)
+writeTracksToExcel(results, 'Alice in Chains')
+writeTracksToExcel(results2, 'Pearl Jam')
+writeTracksToExcel(results3, 'Soundgarden')

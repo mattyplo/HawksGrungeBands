ba90387e20c5f6dc0d42679e4839fe896108a1d1
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-04-03 08:36:04 -0700

    created gitignore and added cred.py to it

--- GrungeBandPopularity.py
+++ GrungeBandPopularity.py
@@ -3,3 +3,6 @@ import pprint
 import xlwt
 from spotipy.oauth2 import SpotifyClientCredentials
 
+
+client_credentials_manager = SpotifyClientCredentials(client_id='', client_secret= '')
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)

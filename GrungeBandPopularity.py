ee0053a3d3142b4bcb00eb1c7083caf3b36ad93a
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-04-03 08:47:15 -0700

    made a call to spotify api to gather track info for a given artist.  Take the track info and create a spreadsheet with it

--- GrungeBandPopularity.py
+++ GrungeBandPopularity.py
@@ -1,8 +1,25 @@
 import spotipy
 import pprint
 import xlwt
+import cred
 from spotipy.oauth2 import SpotifyClientCredentials
 
-
-client_credentials_manager = SpotifyClientCredentials(client_id='', client_secret= '')
+# Setup authorization token to access spotify api
+client_credentials_manager = SpotifyClientCredentials(client_id='926b2b6950e745cc80d89ed6037db878', client_secret= 'd745522d16f747c79e15c2a4648b388f')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+# search spotify for top fifty tracks by given artist
+results = sp.search(q='artist:' + 'alice in chains', type='track', limit=50)
+
+wb = xlwt.Workbook()
+sheet = wb.add_sheet("New Sheet")
+count = 1
+for i in results['tracks']['items']:
+    print(i['name'] + ' ', i['popularity'])
+    name = i['name']
+    print(str(name))
+    sheet.write(count, 1, i['name'])
+    count += 1
+
+wb.save('newSheet.xls')
